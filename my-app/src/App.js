b1ceb8135be1d46a56f0cd64a8bfca75118a06f4
Calla Lu <46143943+clu2001@users.noreply.github.com>
2023-10-25 00:42:56 -0400

    made cursor more interesting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,43 @@
-import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
+import React from 'react';
+import { useRef } from 'react'; 
+import './App.css';
 
 function App() {
-  return (
-    <div id="canvas-container">
-      <Canvas> 
-        <mesh />
-      </Canvas>
-    </div>
-  )
+
+    const cursor = useRef(null)
+    const changePosition = (e) => {
+        cursor.current.style.top = `${e.clientY}px`;
+        cursor.current.style.left = `${e.clientX}px`;
+    }
+
+    const handleMouseEnter = () => {
+        cursor.current.style.width = '40px';
+        cursor.current.style.height = '40px';
+    }
+
+    const handleMouseLeave = () => {
+        cursor.current.style.width = '30px';
+        cursor.current.style.height = '30px';
+    }
+
+    return (
+        <div onMouseMove={changePosition}>
+        <div className="cursor-style" ref={cursor}></div>
+        <div className="intro-container">
+            <div className="intro-content">
+                <h1>Hey there!</h1>
+                <p>My name is Calla, I'm a 4th year CS student at McGill who's passionate about art and development :D</p>
+                <a href="https://github.com/clu2001">
+                    <button href="https://github.com/clu2001" 
+                        onMouseEnter={handleMouseEnter} 
+                        onMouseLeave={handleMouseLeave}>
+                            GitHub Link
+                    </button>
+                </a>
+            </div>
+        </div>
+        </div>
+    );
 }
 
-createRoot(document.getElementById('root')).render(<App />)
-
- export default App
+export default App;
