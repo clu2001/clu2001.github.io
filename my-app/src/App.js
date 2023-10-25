8d3b533d2c57b61cbe4383baa0bd0ba236d1724c
Calla Lu <46143943+clu2001@users.noreply.github.com>
2023-10-25 13:44:32 -0400

    finished basic website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,12 +10,6 @@ function App() {
     const changePosition = (e) => {
         cursor.current.style.top = `${e.clientY}px`;
         cursor.current.style.left = `${e.clientX}px`;
-
-        const tiltAmount = 20;
-        const xTilt = (window.innerHeight / 2 - e.clientY) / tiltAmount;
-        const yTilt = (e.clientX - window.innerWidth / 2) / tiltAmount;
-
-        introContent.current.style.transform = `rotateX(${xTilt}deg) rotateY(${yTilt}deg)`;
     }
 
     const handleMouseEnter = () => {
@@ -28,6 +22,10 @@ function App() {
         cursor.current.style.height = '30px';
     }
 
+    const openPdf = () => {
+        window.open('/resume.pdf', '_blank');
+      };
+
     return (
         <div onMouseMove={changePosition}>
         <div className="cursor-style" ref={cursor}></div>
@@ -36,12 +34,13 @@ function App() {
                 <h1>Hey there!</h1>
                 <p>My name is Calla, I'm a 4th year CS student at McGill who's passionate about art and development :D</p>
                 <a href="https://github.com/clu2001">
-                    <button href="https://github.com/clu2001" 
-                        onMouseEnter={handleMouseEnter} 
-                        onMouseLeave={handleMouseLeave}>
-                            GitHub Link
+                    <button onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                        GitHub Link
                     </button>
                 </a>
+                <button onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={openPdf}>
+                    Resume
+                </button>
             </div>
         </div>
         </div>
